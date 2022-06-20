768972290da03c53d8fce996682c7eeaf17eb176
Iago Pelichek <pelichek.iago@gmail.com>
2022-06-20 16:16:28 -0300

    fix

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import br from '../../public/br.svg'
+import us from '../../public/us.svg'
 
 const languageOptions = [
   {
